4268b4b05005b660e2f6d4f610cd1fc69a1ef33a
aditya <adityakadali00@gmail.com>
2022-11-23 23:57:39 +0530

    added search controller and route

---
+++ controllers/searchController.js
@@ -0,0 +1,22 @@
+const { TodoModel } = require("../models/todo");
+
+const searchControl = async (req, res) => {
+  try {
+    const { query } = req.body;
+    const s = await TodoModel.aggregate().search({
+      index: "todo",
+      text: {
+        query: query,
+        path: ["title", "tasks"],
+      },
+    });
+    res.status(200).json(s);
+  } catch (error) {
+    res.status(503).json({
+      status: 503,
+      error: error,
+    });
+  }
+};
+
+module.exports = { searchControl };
